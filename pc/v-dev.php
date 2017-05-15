47524b1705abed59ad5bdf4ee17065c68cac728a
zeromike <540187842@qq.com>
2017-05-15 22:41:01 +0800

    pc dev

---
+++ pc/v-dev.php
@@ -0,0 +1,22 @@
+<?php
+
+return '{
+    "comment": {
+        "css": [
+            "http://js.pre.meixincdn.com/CDN8152/dist/css/comment-8ba4fc5.css"
+        ],
+        "js": [
+            "http://js.pre.meixincdn.com/CDN8152/dist/js/vendor-26c6415.js",
+            "http://js.pre.meixincdn.com/CDN8152/dist/js/comment-68b9ebf.js"
+        ]
+    },
+    "index": {
+        "css": [
+            "http://js.pre.meixincdn.com/CDN8152/dist/css/index-f0e550f.css"
+        ],
+        "js": [
+            "http://js.pre.meixincdn.com/CDN8152/dist/js/vendor-26c6415.js",
+            "http://js.pre.meixincdn.com/CDN8152/dist/js/index-64fb33f.js"
+        ]
+    }
+}';
